3cddf5a3e82b8043ed6629ae930f4774737531c6
Emaad Paracha <emaad@emaadparacha.com>
2024-10-20 19:42:07 -0400

    space

--- src/getdata.rs
+++ src/getdata.rs
@@ -45,7 +45,6 @@ impl DirFile {
         unsafe { gd_native_type(self.dirfile_open, field_code_ptr) }
     }
 
-
     // Function to get the data of a field in a DirFile and return as Vec<f64>
     pub fn get_data(&self, field: &str) -> Vec<f64> {
         let field_type = self.field_type(field);
