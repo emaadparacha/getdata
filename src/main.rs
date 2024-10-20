2c9203a7a7fbc6186bf7cf4020d5ff096f9219d4
Emaad Paracha <emaad@emaadparacha.com>
2024-10-20 16:22:01 -0400

    main.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+// getdata library
+use getdata::*;
+mod getdata;
+
+// CString
+use std::ffi::CString;
+use std::ffi::CStr;
+
 fn main() {
     println!("Hello, world!");
+
+    let dirfile = CString::new("/data/flights/superbit_2023/timestreams/master_2023-05-03-07-24-30")
+        .expect("CString::new failed");
+    let dirfile_ptr = dirfile.as_ptr();
+
+    let dirfile_open = unsafe { gd_open(dirfile_ptr, GD_RDONLY) };
+    println!("dirfile_open: {:?}", dirfile_open);
+
+    // Find the number of fields in the dirfile
+    let nfields = unsafe { gd_nfields(dirfile_open) };
+
+    // Get the field code for "period_fsc1"
+    let lon_code = CString::new("lon").expect("CString::new failed");
+    let lon_code_ptr = lon_code.as_ptr();
+
+    // Get the entry type for the field "period_fsc1"
+    let lon_type = unsafe { gd_entry_type(dirfile_open, lon_code_ptr) };
+
+    println!("nfields: {:?}", nfields);
+    println!("lon_type: {:?}", lon_type);
+
+    // Allocate space for Lon data
+    let mut lon_data: Vec<f64> = vec![0.0; 100*100];
+
+    // Get all the data of the field "period_fsc1" and store it in the vector
+    let lon_data_size = unsafe {
+        gd_getdata(
+            dirfile_open,
+            lon_code_ptr,
+            1,
+            1,
+            100,
+            100,
+            gd_type_t_GD_FLOAT64, // Use GD_FLOAT64 to match the f64 data type
+            lon_data.as_mut_ptr() as *mut ::std::os::raw::c_void,
+        )
+    };
+
+    // Print the first 200 elements of the vector (or as many as available)
+    let to_print = lon_data.iter().take(200);
+    for (i, value) in to_print.enumerate() {
+        println!("lon_data[{}]: {:?}", i, value);
+    }
+
+    // Print size of lon_data
+    println!("lon_data_size: {:?}", lon_data_size);
+
+
+    // Also get the list of fields
+    let field_list = unsafe { gd_field_list(dirfile_open) };
+
+    // Check if the field_list is not null
+    if !field_list.is_null() {
+        let mut index = 0;
+
+        // Iterate through the list of fields
+        loop {
+            // Get the pointer to the current field
+            let field_ptr = unsafe { *field_list.add(index) };
+            if field_ptr.is_null() {
+                break; // End of the list
+            }
+
+            // Convert the C string to a Rust string
+            let field_name = unsafe { CStr::from_ptr(field_ptr) };
+            match field_name.to_str() {
+                Ok(name) => println!("Field {}: {}", index, name),
+                Err(e) => eprintln!("Failed to convert field name: {}", e),
+            }
+
+            index += 1;
+        }
+    } else {
+        println!("No fields found in dirfile.");
+    }
+
 }
